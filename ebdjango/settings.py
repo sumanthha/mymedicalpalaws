bd54135b9674d18e9e9e4e2df231f6390a35abb7
sumanthha <sumanth.ha@gmail.com>
2019-03-17 00:35:10 -0700

    update home page

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -79,15 +79,8 @@ WSGI_APPLICATION = 'ebdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'OPTIONS': {
-        #    'options': '-c search_path=mymedicalpal'
-        #},
-        #'NAME': 'mymedicalpal',
-        #'USER': 'django',
-        #'PASSWORD': 'django',
-        #'HOST': 'localhost',
-        #'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
