dbb210e4c4df5a01bf468565a36e2f4d42b64f6d
sumanthha <sumanth.ha@gmail.com>
2019-03-17 10:25:03 -0700

    update static files URL

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -122,6 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = '/main/static'
 
 AUTH_USER_MODEL = 'auth.User'
